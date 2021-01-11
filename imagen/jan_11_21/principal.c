426c7531a82644f1ee626a13bb1b724c9f4d0bc8
GaboGomez09 <gomezgabriel09@gmail.com>
2021-01-11 13:31:34 -0600

    implemented sobel

--- imagen/jan_11_21/principal.c
+++ imagen/jan_11_21/principal.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include "imagen.h"
-#define DIMASK 5
+#define DIMASK 3
 
 void RGBToGray( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
 void GrayToRGB( unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height );
 void brilloImagen( unsigned char *imagenGray, uint32_t width, uint32_t height );
 void filtroPB ( unsigned char *imagenG, unsigned char *imagenF, uint32_t width, uint32_t height );
+void Sobel( unsigned char *imagenG, unsigned char *imagenS, uint32_t width, uint32_t height );
 
 unsigned char * reservarMemoria( uint32_t width, uint32_t height );
 
@@ -26,11 +28,11 @@ int main( )
 	RGBToGray( imagenRGB, imagenGray, info.width, info.height );
 
 	//brilloImagen( imagenGray, info.width, info.height );
-	filtroPB( imagenGray, imagenFiltrada, info.width, info.height );
+	Sobel( imagenGray, imagenFiltrada, info.width, info.height );
 
 	GrayToRGB( imagenRGB, imagenFiltrada, info.width, info.height );
 
-	guardarBMP("huella1Gauss.bmp", &info, imagenRGB );
+	guardarBMP("huella1Sobel.bmp", &info, imagenRGB );
 
 	free( imagenRGB );
 	free( imagenGray );
@@ -39,33 +41,66 @@ int main( )
 	return 0;
 }
 
-void filtroPB( unsigned char *imagenG, unsigned char *imagenF, uint32_t width, uint32_t height )
+void Sobel( unsigned char *imagenG, unsigned char *imagenS, uint32_t width, uint32_t height )
 {
 	register int x, y, xm, ym;
-	int indicem, indicei, conv, factor = 330;
-	int kernelGaussiano[DIMASK*DIMASK] = {
-		1, 4,   7,  4, 1,
-		4, 20, 33, 20, 4,
-		7, 33, 54, 33, 7,
-		4, 20, 33, 20, 4,
-		1, 4,   7,  4, 1,
+	int indicem, indicei, convFila, convCol, factor = 4;
+	int gradienteFila[DIMASK*DIMASK] = {
+		1, 0, -1,
+		2, 0, -2,
+		1, 0, -1
+	};
+	int gradienteColumna[DIMASK*DIMASK] = {
+		-1, -2, -1,
+		0, 0, 0,
+		1, 2, 1
 	};
 	for( y = 0; y <= height-DIMASK; y++ )
 		for( x = 0; x <= width-DIMASK; x++ )
 		{
 			indicem = 0;
-			conv = 0;
+			convFila = 0;
+			convCol = 0;
 			for( ym = 0; ym < DIMASK; ym++ )
 				for( xm = 0; xm < DIMASK; xm++ )
 				{
 					indicei = (y+ym)*width + (x+xm);
-					conv += imagenG[indicei] * kernelGaussiano[indicem++];
+					convFila += imagenG[indicei] * gradienteFila[indicem];
+					convCol += imagenG[indicei] * gradienteColumna[indicem++];
 				}
-			conv = conv / factor;
-			indicei = (y+2)*width + (x+2);
-			imagenF[indicei] = conv;
+			convFila = convFila / factor;
+			convCol = convCol / factor;
+			indicei = (y+1)*width + (x+1);
+			imagenS[indicei] = (unsigned char)sqrt((convFila * convFila) + (convCol * convCol));
 		}
 }
+// void filtroPB( unsigned char *imagenG, unsigned char *imagenF, uint32_t width, uint32_t height )
+// {
+// 	register int x, y, xm, ym;
+// 	int indicem, indicei, conv, factor = 330;
+// 	int kernelGaussiano[DIMASK*DIMASK] = {
+// 		1, 4,   7,  4, 1,
+// 		4, 20, 33, 20, 4,
+// 		7, 33, 54, 33, 7,
+// 		4, 20, 33, 20, 4,
+// 		1, 4,   7,  4, 1,
+// 	};
+// 	for( y = 0; y <= height-DIMASK; y++ )
+// 		for( x = 0; x <= width-DIMASK; x++ )
+// 		{
+// 			indicem = 0;
+// 			conv = 0;
+// 			for( ym = 0; ym < DIMASK; ym++ )
+// 				for( xm = 0; xm < DIMASK; xm++ )
+// 				{
+// 					indicei = (y+ym)*width + (x+xm);
+// 					conv += imagenG[indicei] * kernelGaussiano[indicem++];
+// 				}
+// 			conv = conv / factor;
+// 			indicei = (y+2)*width + (x+2);
+// 			imagenF[indicei] = conv;
+// 		}
+// }
 
 void brilloImagen( unsigned char *imagenGray, uint32_t width, uint32_t height )
 {
